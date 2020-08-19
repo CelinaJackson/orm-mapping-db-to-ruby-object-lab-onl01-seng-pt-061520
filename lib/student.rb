1af7c069ef8b0b2482771f4607ec8a95cfd62424
Celina Jackson <celinanjackson@gmail.com>
2020-08-19 22:21:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,7 +70,8 @@ class Student
 
   def self.students_below_12th_grade
     sql = <<-SQL
-       SELECT COUNT(grade > 10) FROM students
+       SELECT COUNT(grade > 10) 
+       FROM students
     SQL
     DB[:conn].execute(sql)
     
